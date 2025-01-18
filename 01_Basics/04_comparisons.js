a83ac51845442f3d571fee0100ebd480a0a95f18
Learning-beast <154521222+Learning-beast@users.noreply.github.com>
2025-01-18 14:33:10 +0000

    comparisons

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,10 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//we are not allowed to compare different data types with each other as it will result in unpredicatable result
+
+console.log("2" > 1);
+console.log("02" > 1);
