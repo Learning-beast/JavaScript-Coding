78ebebe1c4e030ca7fa827fc0df0e96c4fd10439
Learning-beast <154521222+Learning-beast@users.noreply.github.com>
2025-01-18 12:36:01 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+//alert method in javascript result in an error in github to run as there is a specific syntax we are suing nodejs, not browser
+
+//code should be easily readable it should be high
+console.log(3 + 3);
+
+console.log("Harsh Singh Parihar")
+
+
+let name = "harsh" //string data type
+
+let age = 20   //age 
+
+let isLoggedIn = true //or false
+
+//Primitive data type
+// number => 2 to power of 53
+//bigint is used 
+//string => ""
+//boolean = it return either true or false yes or no   true/false
+//null = standalone value /empty value
+//undefined => when you have not assigned any value to the variable
+
+
+//objects
+
+//typeof operator is used to know the type of the variable either in the form of string number float etc
+
+console.log(typeof "harsh");
+console.log(typeof 2342);
+
+console.log(typeof null); //it will output the object as type while undefined type of will result in undefiined no changes
+
+console.log(typeof undefined); //undefined
