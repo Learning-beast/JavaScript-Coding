4c5c9af0c6023ddecf00709e4bfbf7515a40ac34
Harsh Singh Parihar <154521222+Learning-beast@users.noreply.github.com>
2025-01-21 13:57:15 +0530

    Update 05_strings.js

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -63,5 +63,49 @@ const anotherString = gameName.slice(0, 4) ; //we can give both negative values 
 
 const anotherString1 = gameName.slice(-1, -10) ;
 
-//later
+//trimmethod or function in strings it trims or removes the extra spaces before and after the string
 
+const newStringOne = "  hitesh  " ;
+
+console.log(newStringOne);
+
+console.log(newStringOne.trim());
+
+
+//replace function in strings is used to replace the value we want to change for example in below example we replaced the %20 value in url with '-'   syntax of replace fuinction .replace('the value we want to change','with the value we want in place of it' )
+
+const url = "https://harsh.com/harshs%20singh" ;
+
+console.log(url.replace('%20','-'));
+
+
+
+//function to check wheather the value is present in the strings or not = INCLUDES function
+
+console.log(url.includes('harsh')); //as harsh is present in the string it return true lets go below to see if i put any wrong value wheather it will return false or not
+
+console.log(url.includes('sundar')) ; //OUTPUTS FALSE
+
+
+//concat function = to concatenate two strings we can use concat function 
+/*
+Description
+The concat() method joins two or more strings.
+
+The concat() method does not change the existing strings.
+
+The concat() method returns a new string.
+
+*/
+
+let text1 = "sea";
+let text2 = "food";
+let result = text1.concat(text2);
+console.log(result);
+
+//now if we want to output with space in between we use this 
+
+let text3 = "sea";
+let text4 = "food";
+let result1 = text1.concat(" ",text4);
+console.log(result1);
